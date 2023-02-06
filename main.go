e451e9689f3de3a1f4d4246235e69518c3c6094a
Routhu Siddhartha <92938829+siddhuR@users.noreply.github.com>
2023-02-06 22:31:41 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/siddhuR/go-Currency-Exchange/database"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// for json
+	/*
+			viper.AddConfigPath("./configs")
+			viper.SetConfigName("config")
+			viper.SetConfigType("json")
+
+			viper.ReadInConfig()
+
+		fmt.Println(" the viper parameter ", viper.Get("dbname"))
+	*/
+
+	// for .env file
+	/*
+		viper.SetConfigFile(".env")
+
+		viper.ReadInConfig()
+
+		fmt.Println(" the viper parameter ", viper.Get("DBNAME"))
+	*/
+
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+
+	viper.ReadInConfig()
+
+	user := viper.Get("user")
+	pass := viper.Get("pass")
+	dbName := viper.Get("dbname")
+
+	database.RegisterDatabase(user.(string), pass.(string), dbName.(string))
+	// _, _ = utils.DownloadXML("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")
+	database.LoadXMLInDatabase()
+}
